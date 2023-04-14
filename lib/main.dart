6e874c2eecb5bcb5f6452db8e43d656c7e1a8161
azuma <m92fsig38@gmail.com>
2023-04-14 16:06:45 +0900

    Initial value correction

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 99;
+  int _counter = 0;
 
   void _incrementCounter() {
     setState(() {

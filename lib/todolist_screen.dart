62587d1e35fed4ca49c1c2a6df04076b63f2f763
ユーザー名 <azuma_takuya@retail-ai.jp>
2023-04-26 17:33:14 +0900

    メソッド化、仕様変更

--- lib/todolist_screen.dart
+++ lib/todolist_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class TodolistScreen extends StatefulWidget {
-  const TodolistScreen({Key? key}) : super(key: key);
+  const TodolistScreen({super.key});
 
   @override
   _TodoListState createState() => _TodoListState();
@@ -9,17 +9,92 @@ class TodolistScreen extends StatefulWidget {
 
 class _TodoListState extends State<TodolistScreen> {
   final _formKey = GlobalKey<FormState>();
+  final _editKey = GlobalKey<FormState>();
   final List<String> _todos = [];
-  final List<bool> _isEditing = [];
 
   final TextEditingController _controller = TextEditingController();
+  final TextEditingController _editController = TextEditingController();
 
+  void deleteTask(int index) {
+    setState(() {
+      _todos.removeAt(index);
+    });
+  }
+
+  void addTask(String value) {
+    setState(() {
+      _todos.add(value);
+    });
+    _controller.clear();
+  }
+
+  void editTask(String value, int index) {
+    _editController.text = _todos[index];
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('編集'),
+          content: Form(
+            key: _editKey,
+            child: TextFormField(
+              controller: _editController,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'タイトルを入力してください';
+                } else if (_todos.contains(value)) {
+                  return 'このデータはすでに存在します';
+                }
+                return null;
+              },
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                if (_editKey.currentState!.validate()) {
+                  setState(() {
+                    _todos[index] = _editController.text;
+                  });
+                  Navigator.of(context).pop();
+                }
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  //   return TextFormField(
+  //     key: Key(_todos[index]),
+  //     controller: TextEditingController(text: _todos[index]),
+  //     validator: (value) {
+  //       if (value == null || value.isEmpty) {
+  //         return 'タイトルを入力してください';
+  //       }
+  //       if (_todos.contains(value)) {
+  //         return 'このデータはすでに存在します';
+  //       }
+  //       return null;
+  //     },
+  //
+  //     onFieldSubmitted: (newValue) {
+  //       if (_formKey.currentState!.validate()) {
+  //         setState(() {
+  //           _todos[index] = newValue;
+  //         });
+  //       }
+  //     },
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Todo List'),
+        title: const Text('Todo List'),
       ),
       body: Form(
         key: _formKey,
@@ -39,73 +114,39 @@ class _TodoListState extends State<TodolistScreen> {
                 }
                 return null;
               },
-              onChanged: (value) {
-                // 値が変更されるたびにvalidationを実行
-                setState(() {
-                  _formKey.currentState!.validate();
-                });
-              },
-
               onFieldSubmitted: (value) {
                 if (_formKey.currentState!.validate()) {
-                  setState(() {
-                    _todos.add(value);
-                    _isEditing.add(false); // 編集状態のリストに追加
-                    _controller.clear();
-                  });
+                  addTask(value);
                 }
               },
-            ) ,
+            ),
             Expanded(
               child: ListView.builder(
                 itemCount: _todos.length,
                 itemBuilder: (context, index) {
-                  if (_isEditing[index]) {
-                    // 編集状態の場合、テキストフィールドを表示
-                    return TextFormField(
-                      key: Key(_todos[index]),
-                      controller: TextEditingController(text: _todos[index]),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'タイトルを入力してください';
-                        }
-                        if (_todos.contains(value)) {
-                          return 'このデータはすでに存在します';
-                        }
-                        return null;
-                      },
-                      onFieldSubmitted: (newValue) {
-                        if (_formKey.currentState!.validate()) {
-                          setState(() {
-                            _todos[index] = newValue;
-                            _isEditing[index] = false;
-                          });
-                        }
-                      },
-                    );
-                  } else {
-                    // 通常のリストアイテム表示
-                    return ListTile(
-                      key: Key(_todos[index]),
-                      title: Text(_todos[index]),
-                      onTap: () {
-                        // ボタンクリックで編集フラグ切り替え
-                        setState(() {
-                          _isEditing[index] = true;
-                        });
-                      },
-                      trailing: IconButton(
-                        icon: const Icon(Icons.delete),
-                        //アイコンクリックで削除
-                        onPressed: () {
-                          setState(() {
-                            _todos.removeAt(index);
-                            _isEditing.removeAt(index);
-                          });
+                  // 編集状態の場合、テキストフィールドを表示
+                  return Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // 通常のリストアイテム表示
+                      Expanded(
+                          child: ListTile(
+                        key: Key(_todos[index]),
+                        title: Text(_todos[index]),
+                        onTap: () {
+                          // ボタンクリックで編集フラグ切り替え
+                          editTask(_todos[index], index);
                         },
-                      ),
-                    );
-                  }
+                        trailing: IconButton(
+                          icon: const Icon(Icons.delete),
+                          //アイコンクリックで削除
+                          onPressed: () {
+                            deleteTask(index);
+                          },
+                        ),
+                      ))
+                    ],
+                  );
                 },
               ),
             ),

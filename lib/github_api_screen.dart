d4c77023eb9d13a16bbe72d64315c5363bcf08a3
ユーザー名 <azuma_takuya@retail-ai.jp>
2023-05-29 15:56:07 +0900

    Freezed導入 値の取得失敗

--- lib/github_api_screen.dart
+++ lib/github_api_screen.dart
@@ -1,4 +1,3 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'api/github_api.dart';
 import 'model/issues.dart';
@@ -14,20 +13,32 @@ class GithubApiScreen extends StatefulWidget {
 class GithubApiState extends State<GithubApiScreen> {
   final TextEditingController controller1 = TextEditingController();
   final TextEditingController controller2 = TextEditingController();
-  // final Dio dio = Dio();
   late GithubApi githubApi;
 
-  List<Issues>? issues;
+  List<PullRequests>? _pulls;
+
+  // List<Issues>? issues;
   List<PullRequests>? pullRequests;
 
   Future<void> fetchIssuesAndPullRequests() async {
-    print('githubApi: $githubApi');
     String username = controller1.text;
     String projectName = controller2.text;
 
-    issues = await githubApi.getIssues(username, projectName);
+    // issues = await githubApi.getIssues(username, projectName);
     pullRequests = await githubApi.getPullRequests(username, projectName);
-    setState(() {}); // データが更新されたので、UIを再構築するためにsetStateを呼び出します。
+
+    setState(() {
+      // _pulls = pullRequests;
+    }); // データが更新されたので、UIを再構築するためにsetStateを呼び出します。
+  }
+
+  @override
+  void dispose() {
+    // コントローラを破棄します
+    controller1.dispose();
+    controller2.dispose();
+
+    super.dispose();
   }
 
   @override
@@ -54,60 +65,21 @@ class GithubApiState extends State<GithubApiScreen> {
             onPressed: fetchIssuesAndPullRequests,
             child: Text('検索'),
           ),
-          if (issues != null) Text('取得したIssue: ${issues!.length}'),
-          // ここでissuesを表示します。
-          if (pullRequests != null)
-            Text('取得したPullRequest: ${pullRequests!.length}'),
-          // ここでpullRequestsを表示します。
+          const Text('プルリクエスト', style: TextStyle(fontSize: 40)),
+          // Flexible(
+          //   child: ListView.builder(
+          //     itemCount: _pulls?.length,
+          //     itemBuilder: (context, index) {
+          //       return _pulls?[index].title != null
+          //           ? ListTile(
+          //               title: Text(_pulls?[index].title ?? 'プルリクエストがありません'),
+          //             )
+          //           : const SizedBox.shrink();
+          //     },
+          //   ),
+          // ),
         ],
       ),
     );
   }
 }
-
-
-
-// import 'package:flutter/material.dart';
-//
-// class GithubApiScreen extends StatefulWidget {
-//   const GithubApiScreen({super.key});
-//
-//   @override
-//   GithubApiState createState() => GithubApiState();
-// }
-//
-// class GithubApiState extends State<GithubApiScreen> {
-//   final TextEditingController controller1 = TextEditingController();
-//   final TextEditingController controller2 = TextEditingController();
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('GItリポジトリ情報取得'),
-//       ),
-//       body: Column(
-//         children: <Widget>[
-//           TextField(
-//             controller: controller1,
-//             decoration: const InputDecoration(
-//               hintText: 'ユーザー名入力',
-//             ),
-//           ),
-//           TextField(
-//             controller: controller2,
-//             decoration: const InputDecoration(
-//               hintText: 'プロジェクト名入力',
-//             ),
-//           ),
-//           ElevatedButton(
-//             onPressed: () {
-//               // 検索ボタンが押されたときの処理
-//             },
-//             child: const Text('検索'),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
--- lib/model/issues.dart
+++ lib/model/issues.dart
@@ -8,14 +8,14 @@ part 'issues.g.dart';
 // モデル定義
 @freezed
 class Issues with _$Issues {
-  const Issues._(); // メソッドを追加するので、コンストラクタを追加
-  const factory Issues({required String name}) = _Issues;
+  // const Issues._(); // メソッドを追加するので、コンストラクタを追加
+  const factory Issues({required String title}) = _Issues;
 
   factory Issues.fromJson(Map<String, dynamic> json) => _$IssuesFromJson(json);
   // クラスメソッドに、fromJsonStrメソッドを追加
   // factory Issues.fromJsonStr(String jsonStr) => _$IssuesFromJson(json.decode(jsonStr));
 
-  String toJsonStr() {
-    return json.encode(toJson());
-  }
+  // String toJsonStr() {
+  //   return json.encode(toJson());
+  // }
 }

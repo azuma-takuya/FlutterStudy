42524895104c1223d39a479c013edaadb7b76f9f
ユーザー名 <azuma_takuya@retail-ai.jp>
2023-05-29 15:56:07 +0900

    Freezed導入 値の取得失敗

--- lib/model/pull_requests.dart
+++ lib/model/pull_requests.dart
@@ -8,14 +8,8 @@ part 'pull_requests.g.dart';
 // モデル定義
 @freezed
 class PullRequests with _$PullRequests {
-  const PullRequests._(); // メソッドを追加するので、コンストラクタを追加
-  const factory PullRequests({required String name}) = _PullRequests;
+  // const PullRequests._(); // メソッドを追加するので、コンストラクタを追加
+  const factory PullRequests({required String title}) = _PullRequests;
 
   factory PullRequests.fromJson(Map<String, dynamic> json) => _$PullRequestsFromJson(json);
-  // クラスメソッドに、fromJsonStrメソッドを追加
-  // factory PullRequests.fromJsonStr(String jsonStr) => _$PullRequestsFromJson(json.decode(jsonStr));
-
-  String toJsonStr() {
-    return json.encode(toJson());
-  }
 }

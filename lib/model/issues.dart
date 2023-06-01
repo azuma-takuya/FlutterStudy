8a2294e84a8ecb80701e8552edbc1a0aef13d07a
ユーザー名 <azuma_takuya@retail-ai.jp>
2023-06-01 16:01:30 +0900

    issue情報取得途中

--- lib/model/issues.dart
+++ lib/model/issues.dart
@@ -1,14 +1,14 @@
-// import 'package:flutter/foundation.dart';
-// import 'package:freezed_annotation/freezed_annotation.dart';
-//
-// part 'issues.freezed.dart';
-// part 'issues.g.dart';
-//
-// // モデル定義
-// @freezed
-// class Issues with _$Issues {
-//   // const Issues._(); // メソッドを追加するので、コンストラクタを追加
-//   const factory Issues({required String title}) = _Issues;
-//
-//   factory Issues.fromJson(Map<String, dynamic> json) => _$IssuesFromJson(json);
-// }
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'issues.freezed.dart';
+part 'issues.g.dart';
+
+// モデル定義
+@freezed
+class Issues with _$Issues {
+  // const Issues._(); // メソッドを追加するので、コンストラクタを追加
+  const factory Issues({required String title}) = _Issues;
+
+  factory Issues.fromJson(Map<String, dynamic> json) => _$IssuesFromJson(json);
+}

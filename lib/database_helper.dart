fee0a5b98ac2b3142bde4dcd2c44ec4edbf6d9f7
ユーザー名 <azuma_takuya@retail-ai.jp>
2023-06-06 15:56:47 +0900

    SQLite導入中

---
+++ lib/database_helper.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+
+  DatabaseHelper._privateConstructor();
+
+  static const _databaseName = 'MyDatabase.db';
+  static const _databaseVersion = 1;
+
+  static const table = 'my_table';
+  static const columnId = '_id';
+  static const columnName = 'name';
+  static const columnAge = 'age';
+
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database? _database;
+
+  Future<Database?> get database async {
+    if (_database != null) return _database;
+    _database = await _initDatabase();
+    return _database;
+  }
+
+  // Updated return type to Future<Database>
+  Future<Database> _initDatabase() async {
+    Directory? documentsDirectory = await getApplicationDocumentsDirectory();
+    String?? path = join(documentsDirectory?.path, _databaseName);
+    return await openDatabase(path,
+        version: _databaseVersion,
+        onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+          CREATE TABLE $table (
+            $columnId INTEGER PRIMARY KEY,
+            $columnName TEXT NOT NULL,
+            $columnAge INTEGER NOT NULL
+          )
+          ''');
+  }
---
+++ lib/model/news.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+
+part 'news.g.dart';
+
+// モデル定義
+class News with _$News {
+  // const Issues._(); // メソッドを追加するので、コンストラクタを追加
+  const factory News({required String title}) = _News;
+
+  factory News.fromJson(Map<String, dynamic> json) => _$NewsFromJson(json);
+}

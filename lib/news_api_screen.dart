0ae4fd3fc1bf850c32b36f348a30f7788be5ad4c
ユーザー名 <azuma_takuya@retail-ai.jp>
2023-06-19 15:54:04 +0900

    作業途中

--- lib/news_api_screen.dart
+++ lib/news_api_screen.dart
@@ -1,83 +1,69 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'favorites_manager.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'api/news_api.dart';
+import 'favorites_manager.dart';
 import 'model/news.dart';
 import 'model/news_result.dart';
 import 'news_favorite_screen.dart';
 
+final newsProvider = FutureProvider.autoDispose<NewsResult>((ref) async {
+  final dio = Dio();
+  final newsApi = NewsApi(dio);
+  const newsApiKey = String.fromEnvironment('NEWS_API_KEY');
+  final response = await newsApi.getNews(newsApiKey);
+  return response;
+});
 
-class NewsApiScreen extends StatefulWidget {
+final favoritesProvider = ChangeNotifierProvider((ref) => FavoritesManager());
+
+class NewsApiScreen extends ConsumerWidget {
   const NewsApiScreen({super.key});
 
   @override
-  State<NewsApiScreen> createState() => NewsApiState();
-}
+  Widget build(BuildContext context, ScopedReader watch) {
+    final newsList = watch(newsProvider);
+    final favoritesManager = watch(favoritesProvider);
 
-class NewsApiState extends State<NewsApiScreen> {
-  late NewsApi newsApi;
-  NewsResult? newsList;
-  List<News>? favorites = [];
-
-  @override
-  void initState() {
-    super.initState();
-    final dio = Dio();
-    newsApi = NewsApi(dio);
-    fetchNews();
-    context.read<FavoritesManager>().readFavorites();
-  }
-
-  Future<void> fetchNews() async {
-    try {
-      const newsApiKey = String.fromEnvironment('NEWS_API_KEY');
-      final response =
-      await newsApi.getNews(newsApiKey);
-      setState(() {
-        newsList = response;
-      });
-    } catch (e) {
-      print('Failed to fetch news: $e');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final favoritesManager = Provider.of<FavoritesManager>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('News API Fetch'),
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.refresh),
-            onPressed: fetchNews,
+            onPressed: () => context.refresh(newsProvider),
           ),
           IconButton(
             icon: const Icon(Icons.list),
             onPressed: () {
               Navigator.push(context, MaterialPageRoute(builder: (context) =>
-                const NewsFavoritesScreen(),),
+              const NewsFavoritesScreen(),),
               );
             },
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: newsList?.articles?.length,
-        itemBuilder: (context, index) {
-          final news = newsList?.articles![index];
-          return ListTile(
-            title: Text(news?.title ?? 'No title'),
-            subtitle: Text(news?.url ?? 'No URL'),
-            trailing: IconButton(
-              icon: favoritesManager.isFavorite(news)
-                  ? const Icon(Icons.favorite, color: Colors.red)
-                  : const Icon(Icons.favorite_border),
-              onPressed: () => favoritesManager.toggleFavorite(news!),
+      body: newsList.when(
+        data: (news) =>
+            ListView.builder(
+              itemCount: news.articles.length,
+              itemBuilder: (context, index) {
+                final newsItem = news.articles[index];
+                return ListTile(
+                  title: Text(newsItem.title ?? 'No title'),
+                  subtitle: Text(newsItem.url ?? 'No URL'),
+                  trailing: IconButton(
+                    icon: favoritesManager.isFavorite(newsItem)
+                        ? const Icon(Icons.favorite, color: Colors.red)
+                        : const Icon(Icons.favorite_border),
+                    onPressed: () => favoritesManager.toggleFavorite(newsItem),
+                  ),
+                );
+              },
             ),
-          );
-        },
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (e, stack) => Center(child: Text('Failed to fetch news: $e')),
       ),
     );
   }
